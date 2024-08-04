aa18fc757a2e9e3df026fe3f08888a5fa91a6380
ANUMAREDDY <khyati@gmail.com>
2024-08-04 07:29:16 -0400

    creating a sLL with a single node

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        SingleLinkedList sLL = new SingleLinkedList();
+        sLL.createSingleLinkedList(10);
+        System.out.println(sLL.tail.value);
+    }
+}
